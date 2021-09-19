221e814dac8d78c009ff0daf0d431b1afdf23919
kimsooyoung <tge1375@naver.com>
2021-09-19 23:17:11 +0900

    Feature : joy sub node

--- unitree_legged_real/src/exe/joy_sub_node.cpp
+++ unitree_legged_real/src/exe/joy_sub_node.cpp
@@ -1,6 +1,6 @@
-
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
+#include <string>
 #include <unitree_legged_msgs/HighCmd.h>
 
 // std_msgs/Header header
@@ -10,34 +10,55 @@
 // float32[] axes
 // int32[] buttons
 
-unitree_legged_msgs::HighCmd SendHighROS;
-auto SCALE_GAIN = 0.3f;
+class JoySubA1CmdPub {
+private:
+  std::string m_name;
 
-void sub_callback(const sensor_msgs::Joy &data) {
-  std::cout << "1X : " << data.axes[0] << std::endl;
-  std::cout << "1Y : " << data.axes[1] << std::endl;
-  std::cout << "2X : " << data.axes[3] << std::endl;
-  std::cout << "2Y : " << data.axes[4] << std::endl;
+  unitree_legged_msgs::HighCmd SendHighROS;
+  float SCALE_GAIN = 0.3f;
 
-  std::cout << std::endl;
+  ros::NodeHandle m_nh;
+  ros::Publisher m_pub;
+  ros::Subscriber m_sub;
 
-  SendHighROS.mode = 1;
-  SendHighROS.roll = data.axes[3] * SCALE_GAIN;
-  SendHighROS.pitch = data.axes[4] * SCALE_GAIN;
-  SendHighROS.yaw = data.axes[0] * SCALE_GAIN;
+public:
+  JoySubA1CmdPub(const std::string &name_in) : m_name(name_in) {
+    ROS_INFO("Publisher and Subscriber initialized");
 
-  //   SendHighROS.forwardSpeed = data.axes[1] * SCALE_GAIN;
-  SendHighROS.forwardSpeed = 0.0f;
-  SendHighROS.sideSpeed = 0.0f;
-  SendHighROS.rotateSpeed = 0.0f;
-  SendHighROS.bodyHeight = 0.0f;
-}
+    m_pub = m_nh.advertise<unitree_legged_msgs::HighCmd>("/a1_high_cmd", 5);
+    m_sub = m_nh.subscribe("/joy", 1, &JoySubA1CmdPub::subCallback, this);
+  }
+
+  ~JoySubA1CmdPub() {}
+
+  void subCallback(const sensor_msgs::Joy &data) {
+    std::cout << "1X : " << data.axes[0] << std::endl;
+    std::cout << "1Y : " << data.axes[1] << std::endl;
+    std::cout << "2X : " << data.axes[3] << std::endl;
+    std::cout << "2Y : " << data.axes[4] << std::endl;
+
+    SendHighROS.mode = 1;
+    SendHighROS.roll = data.axes[3] * 0.3;
+    SendHighROS.pitch = data.axes[4] * 0.3;
+    SendHighROS.yaw = data.axes[0] * 0.3;
+
+    //   SendHighROS.forwardSpeed = data.axes[1] * SCALE_GAIN;
+    SendHighROS.forwardSpeed = 0.0f;
+    SendHighROS.sideSpeed = 0.0f;
+    SendHighROS.rotateSpeed = 0.0f;
+    SendHighROS.bodyHeight = 0.0f;
+
+    m_pub.publish(SendHighROS);
+
+    std::cout << SendHighROS.roll << std::endl;
+    std::cout << std::endl;
+  }
+};
 
 int main(int argc, char **argv) {
 
   ros::init(argc, argv, "joy_sub_node");
-  ros::NodeHandle nh;
-  ros::Subscriber sub = nh.subscribe("/joy", 1, sub_callback);
+  JoySubA1CmdPub joy_sub_a1_cmd("a1_controll_with_joy");
 
   ros::spin();
 
